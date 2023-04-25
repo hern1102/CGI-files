4e414555ef0809230aadec65d4feb4a68d305ad7
hern1102 <43502582+hern1102@users.noreply.github.com>
2023-04-25 10:46:36 -0700

    Testing ruby

--- ruby-hello-html-world.rb
+++ ruby-hello-html-world.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/ruby
 
-puts "HTTP/1.0 200 OK"
-puts "Content-type: text/html\n\n"
+require 'cgi'
+cgi = CGI.new
+
+puts cgi.header
+# puts "HTTP/1.0 200 OK"
+# puts "Content-type: text/html\n\n"
 puts "<html><body>This is a test</body></html>"
