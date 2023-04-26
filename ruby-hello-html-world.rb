11b73ea9d90547422b884067b3035029dbfa0928
hern1102 <43502582+hern1102@users.noreply.github.com>
2023-04-25 17:16:08 -0700

    test cgi ruby

--- ruby-hello-html-world.rb
+++ ruby-hello-html-world.rb
@@ -3,7 +3,24 @@
 require 'cgi'
 cgi = CGI.new
 
-puts cgi.header
-# puts "HTTP/1.0 200 OK"
-# puts "Content-type: text/html\n\n"
-puts "<html><body>This is a test</body></html>"
+puts "Cache-Control: no-cache\n";
+puts "Content-type: text/html\n\n"
+puts "<html>";
+puts "<head>";
+puts "<title>Hello, Perl!</title>";
+puts "</head>";
+puts "<body>";
+
+puts "<h1>Team 135 was here - Hello, Ruby!</h1>";
+puts "<p>This page was generated with the Ruby programming langauge</p>";
+
+date = Time.now
+
+puts "<p>Current Time: #{date.localtime}</p>";
+
+ip_address = cgi.REMOTE_ADDR
+
+puts "<p>Your IP Address: #{ip_address}</p>";
+
+puts "</body>";
+puts "</html>";
