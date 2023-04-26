f1c23b6f4c739bb9fb7e0320cf6a20ae29164b5e
hern1102 <43502582+hern1102@users.noreply.github.com>
2023-04-25 22:08:54 -0700

    second ruby file

--- ruby-hello-html-world.rb
+++ ruby-hello-html-world.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/ruby -w
+#!/usr/bin/ruby
 
 ip_address = ENV['REMOTE_ADDR']
 
---
+++ ruby-hello-json-world.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+
+require 'json'
+
+puts "Cache-Control: no-cache\n";
+puts "Content-type: application/json\n\n";
+
+cgi = CGI.new
+
+ip_address = ENV['REMOTE_ADDR']
+
+json_data = {
+    title: 'Hello, Ruby.', 
+    heading: 'Hello, Ruby.',
+    message: 'This page was generated with the Ruby programming language',
+    time: Time.now.localtime,
+    ip_address: ENV['REMOTE_ADDR']
+}
+
+puts json_data
