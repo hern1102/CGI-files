c667b17c428ef9d86cade53e6f2bc549a3a270db
hern1102 <43502582+hern1102@users.noreply.github.com>
2023-04-27 15:19:33 -0700

    Finished general request

--- ruby-general-request-echo.rb
+++ ruby-general-request-echo.rb
@@ -8,8 +8,8 @@ puts "Content-type: text/html \n\n";
 
 # print HTML file top
 puts "<!DOCTYPE html>
-<html><head><title>Post Request Echo</title>
-</head><body><h1 align='center'>Post Request Echo</h1>
+<html><head><title>General Request Echo</title>
+</head><body><h1 align='center'>General Request Echo</h1>
 <hr>"
 
 q_str = ENV['QUERY_STRING']
@@ -23,17 +23,27 @@ puts "<b>http protocol:</b> #{proto}<br />\n";
 puts "<b>request method:</b> #{meth}<br />\n";
 puts "<b>Query String:</b> #{q_str}<br />\n";
 # message body
-puts "<b>Message body:</b><br />\n";
+puts "<b>Message body: </b>";
+
 
 # # The Query String is simply an environment variable
 # puts "<b>Query String:</b> #{q_str}<br />\n";
 
 # # Credit for this code to parse the Query string:
 # # https://www.mediacollege.com/internet/perl/query-string.html
-if q_str < 1
+if q_str.length < 1
+    params_count = cgi.params.length
+    count = 0   
+    out = ""
     params.each do |key, value|
-    puts "<li>#{key} = #{value[0]}</li><br/>\n";
-    end
+        if count < params_count - 1
+            out += "#{key}=#{value[0]}&";
+        else
+            out += "#{key}=#{value[0]}";
+        end
+        count = count + 1
+        end
+    puts "#{out}"
 end 
 
 # Print the HTML file bottom
